fc8e31a6d500e960adfd5f2a920a60ed473183e8
Michael Schreiber <michael.a.schreiber@gmail.com>
2023-06-12 12:55:55 -0400

    Break components into individual files

---
+++ lib/service_tile.dart
@@ -0,0 +1,37 @@
+import 'package:bluetooth_scanner/characteristic_tile.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_blue_plus/flutter_blue_plus.dart';
+
+class ServiceTile extends StatelessWidget {
+  final BluetoothService service;
+  final List<CharacteristicTile> characteristicTiles;
+
+  const ServiceTile(
+      {Key? key, required this.service, required this.characteristicTiles})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    if (characteristicTiles.isNotEmpty) {
+      return ExpansionTile(
+        title: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            const Text('Service'),
+            Text('0x${service.uuid.toString().toUpperCase().substring(4, 8)}',
+                style: Theme.of(context).textTheme.bodyText1?.copyWith(
+                    color: Theme.of(context).textTheme.caption?.color))
+          ],
+        ),
+        children: characteristicTiles,
+      );
+    } else {
+      return ListTile(
+        title: const Text('Service'),
+        subtitle:
+            Text('0x${service.uuid.toString().toUpperCase().substring(4, 8)}'),
+      );
+    }
+  }
+}
